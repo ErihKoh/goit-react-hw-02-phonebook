7f3c6ad5c9617f04fa6cd3545ba764dacf4dc818
Yurii Zubar <barsuk0831@gmail.com>
2020-12-05 21:36:02 +0200

    phonebook

--- src/App.js
+++ src/App.js
@@ -2,16 +2,23 @@ import { Component } from 'react';
 import PropTypes from 'prop-types';
 import './App.css';
 import Section from './components/Section';
+import ContactForm from './components/ContactForm';
+// import contacts from './components/ContactList/contacts';
 
 class App extends Component {
-  state = {};
+  state = {
+    contacts: [],
+    filter: '',
+  };
 
   static propTypes = {};
 
   render() {
     return (
       <>
-        <Section title="Phonebook" />
+        <Section title="Phonebook">
+          <ContactForm />
+        </Section>
         <Section title="Contacts" />
       </>
     );
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,16 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+import s from './ContactForm.module.css';
+
+class ContactForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+  static propTypes = {};
+  render() {
+    return <></>;
+  }
+}
+
+export default ContactForm;
